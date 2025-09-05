b6ffb433a0776d6ebea9f79eea036d2d8cf44276
Innobing <innobingpruebas@gmail.com>
2025-09-05 16:43:05 +0200

    First commit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import {configDefaults} from 'vitest/config'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './testSetup.js',
+    include: ['**/*.{test,spec}.{js,ts,jsx,tsx}'],
+    exclude: [...configDefaults.exclude, 'node_modules', 'dist']
+  },
+})
