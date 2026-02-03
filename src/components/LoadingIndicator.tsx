39c2fb98db38166b80afa9c630d7bb5177338447
Enrique Verea <kikeverea@gmail.com>
2026-02-03 18:16:58 +0100

    Add svg support

---
+++ src/components/LoadingIndicator.tsx
@@ -0,0 +1,11 @@
+import Indicator from '../assets/indicator.svg?react'
+
+const LoadingIndicator = () => {
+  return (
+    <div role='status'>
+      <Indicator />
+    </div>
+  )
+}
+
+export default LoadingIndicator
