4c9165d342301c2ecc082c55fb4480122ce57417
Enrique Verea <kikeverea@gmail.com>
2026-02-05 18:12:39 +0100

    Add tooltip component

---
+++ src/components/Tooltip.tsx
@@ -0,0 +1,37 @@
+import type { ReactNode } from 'react'
+
+export type TooltipType = {
+  title: string,
+  position?: 'top' | 'right' | 'bottom' | 'left',
+}
+
+type TooltipProps = TooltipType & {
+  children: ReactNode,
+  className?: string
+}
+
+const Tooltip = ({ title, position = 'top', children, className }: TooltipProps) => {
+
+  const positionClasses: Record<string, string> = {
+    top: 'bottom-full mb-1 left-1/2 -translate-x-1/2',
+    right: 'left-full ms-2 top-1/2 -translate-y-1/2',
+    bottom: 'top-full mt-1 left-1/2 -translate-x-1/2 mb-1',
+    left: 'right-full me-2 top-1/2 -translate-y-1/2',
+  };
+
+  return (
+    <div className={`relative group overflow-visible ${className}`}>
+      {children}
+      <div className={`
+        absolute ${positionClasses[position]}
+        bg-white text-black px-2 py-0.5 rounded-md text-sm w-max
+        hidden group-hover:block
+        pointer-events-none`}
+      >
+        {title}
+      </div>
+    </div>
+  )
+}
+
+export default Tooltip
--- src/util/types.ts
+++ src/util/types.ts
@@ -1,1 +1,2 @@
 export type FormDataEntries = { [k: string]: FormDataEntryValue }
+export type Optional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
