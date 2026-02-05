3b108ca274e79b76bf8214bea5bc49dcb3e78b6f
Enrique Verea <kikeverea@gmail.com>
2026-02-05 18:36:43 +0100

    Add 'add new' button to lists view

--- src/todos/TodoLists/TodoLists.tsx
+++ src/todos/TodoLists/TodoLists.tsx
@@ -1,6 +1,9 @@
 import TodoListItem from '../TodoList/TodoListItem'
 import useTodoLists from './useTodoLists'
 import LoadingIndicator from '../../components/LoadingIndicator'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import CircleButton from '../../components/CircleButton.tsx'
 
 const TodoLists = () => {
 
@@ -8,22 +11,21 @@ const TodoLists = () => {
   const { pending, errors } = status
 
   return (
-    <div className='flex flex-wrap gap-8 relative'>
-      { lists?.map(list =>
-        <TodoListItem key={list.id} list={list} onRemove={remove}/>)
-      }
-      { pending.creating && <LoadingIndicator /> }
-      <button
-        type='button'
-        className='btn absolute'
-        aria-label='new list'
-        onClick={ create }
-      >
-        Add new list
-      </button>
+    <div className="flex items-start gap-4">
+      <div className='flex flex-wrap gap-8 relative flex-1'>
+        {lists?.map(list =>
+          <TodoListItem key={list.id} list={list} onRemove={remove}/>)
+        }
+        {pending.creating && <LoadingIndicator/>}
+      </div>
+      <CircleButton
+        tooltip={{ title: 'Nueva lista', position: 'left' }}
+        icon={<FontAwesomeIcon icon={faPlus}/>}
+        onClick={create}
+        ariaLabel='new list'
+      />
     </div>
   )
-
 }
 
 export default TodoLists
